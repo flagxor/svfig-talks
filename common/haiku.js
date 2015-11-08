9cb2e01046bf68cf8d6fff93e9708f71b8fef68b
Brad Nelson <flagxor@gmail.com>
2015-11-08 12:32:00 -0800

    Adding touch pad hooks.

--- common/haiku.js
+++ common/haiku.js
@@ -1,3 +1,7 @@
+/***************************************/
+/* Inject Haikus                       */
+/***************************************/
+
 function fixup(i) {
   var url = 'https://forthsalon.appspot.com/haiku-bare/';
   url += i.getAttribute('data-haiku');
@@ -15,3 +19,68 @@ setInterval(function() {
     fixup(haikus[i]);
   }
 }, 500);
+
+
+/***************************************/
+/* Attempt to support touch pad        */
+/***************************************/
+
+var haiku_touch_port = null;
+var haiku_touch_buffer = '';
+
+function connect_touch() {
+  try {
+    haiku_touch_port = chrome.runtime.connect(
+        'gjpkfjbomndhibbiiakfjpgjcaeggbic');
+    haiku_touch_port.onMessage.addListener(function(m) {
+      haiku_touch_buffer += m.data.replace(/[\r]/g, '');
+      var parts = haiku_touch_buffer.split('\n');
+      if (parts.length > 1) {
+        for (var i = 0; i < parts.length - 1; i++) {
+          if (window.onstroke !== undefined &&
+              parts[i].length !== 0) {
+            console.log('stroke: ' + parts[i]);
+            try {
+              window.onstroke(parts[i]);
+            } catch(e) {
+            }
+          }
+        }
+        haiku_touch_buffer = parts[parts.length - 1];
+      }
+    });
+  } catch(e) {
+  }
+}
+
+function send_to_touch(s) {
+  if (haiku_touch_port === null) {
+    return;
+  }
+  try {
+    haiku_touch_port.postMessage({'data': s});
+  } catch(e) {
+  }
+}
+
+window.addEventListener('load', connect_touch);
+
+
+/***************************************/
+/* Attempt to support touch pad        */
+/***************************************/
+window.onstroke = function(s) {
+  if (s === 'R') {
+    Reveal.prev();
+  } else if (s === 'S') {
+    Reveal.next();
+  } else if (s === 'F') {
+    Reveal.left();
+  } else if (s === 'T') {
+    Reveal.right();
+  } else if (s === 'L') {
+    Reveal.up();
+  } else if (s === 'P') {
+    Reveal.down();
+  }
+};
