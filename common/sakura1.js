4c49684ba0d960684ce821f8e292a5ee6edfe7d6
Brad Nelson <flagxor@gmail.com>
2020-10-13 23:01:44 -0700

    Clean up configs.

---
+++ common/sakura1.js
@@ -0,0 +1,7 @@
+Reveal.configure({
+  parallaxBackgroundImage: '../common/sakura1.jpg',
+  parallaxBackgroundSize: '2000px 800px',
+  parallaxBackgroundHorizontal: '400',
+  parallaxBackgroundVertical: '0',
+});
+
