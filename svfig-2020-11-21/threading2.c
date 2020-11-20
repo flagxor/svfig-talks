ca4974db37680c316923681727fa8c15220fac07
Brad Nelson <flagxor@gmail.com>
2020-11-19 23:27:05 -0800

    More

--- svfig-2020-11-21/threading2.c
+++ svfig-2020-11-21/threading2.c
@@ -17,10 +17,13 @@ typedef intptr_t cell_t;
   X(and, tos &= *sp--) \
   X(or, tos |= *sp--) \
   X(dolit, *++sp = tos; tos = *ip++) \
-  X(docolon, *++rp = (cell_t) ip; ip = ((cell_t *) w) + 1) \
-  X(doexit, ip = (cell_t *) *rp--) \
   X(load, tos = * (cell_t *) tos) \
   X(store, *(cell_t *) tos = *sp--; tos = *sp--) \
+
+
+#define THREADING_WORDS \
+  X(docolon, *++rp = (cell_t) ip; ip = ((cell_t *) w) + 1) \
+  X(doexit, ip = (cell_t *) *rp--) \
 
 
 #define COMMON_REGS \
@@ -38,10 +41,12 @@ void indirect_threaded(cell_t *memory, cell_t *dstack, cell_t *rstack) {
 
 #define X(name, code) memory[i++] = (cell_t) && name;
 CORE_WORDS
+THREADING_WORDS
 #undef X
 
 #define X(name, code) name: code; IDT_NEXT;
 CORE_WORDS
+THREADING_WORDS
 #undef X
 }
 
@@ -50,9 +55,36 @@ void direct_threaded(cell_t *memory, cell_t *dstack, cell_t *rstack) {
 
 #define X(name, code) memory[i++] = (cell_t) && name;
 CORE_WORDS
+THREADING_WORDS
 #undef X
 
 #define X(name, code) name: code; DT_NEXT;
 CORE_WORDS
+THREADING_WORDS
 #undef X
+}
+
+enum {
+OP_BEGIN = 0,
+#define X(name, code) OP_ ## name,
+CORE_WORDS
+#undef X
+OP_MAXTOKEN,
+};
+
+void cforth_like(cell_t *memory, cell_t *dstack, cell_t *rstack) {
+  COMMON_REGS;
+  cell_t op;
+
+  for (;;) {
+    op = *ip++;
+    switch (op) {
+#define X(name, code) case OP_ ## name: code; continue;
+CORE_WORDS
+#undef X
+    default:
+      *--rp = (cell_t) ip;
+      ip = (cell_t *) *(cell_t *) (ip - OP_MAXTOKEN);
+    }
+  }
 }
